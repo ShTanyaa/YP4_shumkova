40c985e0585fe5e31eb36e982f83a7f15be9cb66
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-06-20 14:28:50 +0500

    Add files via upload

---
+++ App1/App1/App1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using App1.Services;
+using App1.Views;
+using CreditCalc;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
